31b33855260274a16d2e6df8fcdcbb9f4c0f0613
eyalerez <eyal.erez91@gmail.com>
2017-04-25 19:07:08 +0300

    First commit

---
+++ CyberPC.h.cpp
@@ -0,0 +1,11 @@
+#include "CyberPC.h.h"
+
+CyberPC.h::CyberPC.h()
+{
+    //ctor
+}
+
+CyberPC.h::~CyberPC.h()
+{
+    //dtor
+}
---
+++ CyberPC.h.h
@@ -0,0 +1,14 @@
+#ifndef CYBERPC_H_H
+#define CYBERPC_H_H
+
+
+class CyberPC.h
+{
+    public:
+        CyberPC.h();
+        virtual ~CyberPC.h();
+    protected:
+    private:
+};
+
+#endif // CYBERPC_H_H
